d25825ea2c7fe5005279f3454f8f26cf4235cd1a
theCodeSpeaksForItself <rashidju123@gmail.com>
2025-07-16 22:17:22 +0000

    First commit, outlined functions for the code

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let humanScore = 0;
+
+let computerScore = 0;
+
+
+function getComputerChoice () {} // which returns either rock, paper, scissors using the Math.random method
+
+
+function getHumanChoice () {} // use the prompt method to get user's input
+
+
+function playGame () { // This function can call playRound several times, depending on the amount of rounds they entered to play
+    function playRound (humanChoice, computerChoice) {}
+}
