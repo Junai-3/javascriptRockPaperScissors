20c6f93f0a8371384bcef40aaef27e8fec57a09c
theCodeSpeaksForItself <rashidju123@gmail.com>
2025-07-17 11:52:43 +0000

    added code for human & computer choice

--- script.js
+++ script.js
@@ -4,12 +4,40 @@ let humanScore = 0;
 let computerScore = 0;
 
 
-function getComputerChoice () {} // which returns either rock, paper, scissors using the Math.random method
+function getComputerChoice () {
+    
+    computerChoice = Math.floor(Math.random()*3)
+    
+    if (computerChoice == 0) {return "rock"}
+        
+    else if (computerChoice == 1) {return "paper"}
+        
+    else if (computerChoice == 2) {return "scissors"}
+    
+
+}
+// which returns either rock, paper, scissors using the Math.random method
 
 
-function getHumanChoice () {} // use the prompt method to get user's input
+function getHumanChoice () {
+
+    humanChoice = prompt.toLowerCase(("Rock, Paper, or Scissors?"))
+
+    if (humanChoice == "rock") {return "rock"}
+        
+    else if (humanChoice == "paper") {return "paper"}
+        
+    else if (humanChoice == "scissors") {return "scissors"}
+        
+    else {return "ERROR! Invalid Input!"; getHumanChoice() }
+}
+
+// use the prompt method to get user's input
 
 
-function playGame () { // This function can call playRound several times, depending on the amount of rounds they entered to play
+function playGame () {
+    
+// This function can call playRound several times, depending on the amount of rounds they entered to play
+    
     function playRound (humanChoice, computerChoice) {}
 }
